f167be0c8288324f4844f1a57b2954d6de2c2424
Tom Pizza <tomromaniff@proton.me>
2026-09-11 19:41:34 +0500

    simplify air

--- src/engine/air.rs
+++ src/engine/air.rs
@@ -22,129 +22,59 @@ pub struct AirRoom {
 }
 
 #[inline]
-fn mode_shape_at_position(
-    index_x: usize,
-    index_y: usize,
-    index_z: usize,
-    position: Vector3,
-    inverse_room_x: f64,
-    inverse_room_y: f64,
-    inverse_room_z: f64,
-) -> f64 {
-    (index_x as f64 * PI * position.x * inverse_room_x).cos()
-        * (index_y as f64 * PI * position.y * inverse_room_y).cos()
-        * (index_z as f64 * PI * position.z * inverse_room_z).cos()
+fn simplified_mode_shape(index: usize, normalized_pos: f64) -> f64 {
+    (index as f64 * PI * normalized_pos).cos()
 }
 
 impl AirRoom {
     pub fn new(design: &AirRoomDesign) -> Self {
-        let room = design.room_dimensions;
-        let room_volume = room.x * room.y * room.z;
-        let inverse_room_x = 1.0 / room.x;
-        let inverse_room_y = 1.0 / room.y;
-        let inverse_room_z = 1.0 / room.z;
+        let num_modes = 64;
 
-        let maximum_wavenumber =
-            2.0 * PI * design.highest_modeled_frequency / design.speed_of_sound;
+        let mut modal_states = Vec::with_capacity(num_modes);
+        let mut mode_shape_at_soundboard = Vec::with_capacity(num_modes);
+        let mut mode_shape_at_listener = Vec::with_capacity(num_modes);
+        let mut source_gain_per_volume_velocity = Vec::with_capacity(num_modes);
+        let mut soundboard_pressure_coefficients = Vec::with_capacity(num_modes);
+        let mut listener_pressure_coefficients = Vec::with_capacity(num_modes);
 
-        let maximum_index_x = (maximum_wavenumber * room.x / PI).ceil() as usize;
-        let maximum_index_y = (maximum_wavenumber * room.y / PI).ceil() as usize;
-        let maximum_index_z = (maximum_wavenumber * room.z / PI).ceil() as usize;
+        let norm_pos_sb = design.soundboard_position.x / design.room_dimensions.x;
+        let norm_pos_lis = design.listener_position.x / design.room_dimensions.x;
 
-        let estimated_capacity =
-            (maximum_index_x + 1) * (maximum_index_y + 1) * (maximum_index_z + 1);
+        let base_frequency = design.speed_of_sound / (2.0 * design.room_dimensions.x);
+        let density_c2 = design.air_density * design.speed_of_sound * design.speed_of_sound;
 
-        let mut modal_states = Vec::with_capacity(estimated_capacity);
-        let mut mode_shape_at_soundboard = Vec::with_capacity(estimated_capacity);
-        let mut mode_shape_at_listener = Vec::with_capacity(estimated_capacity);
-        let mut source_gain_per_volume_velocity = Vec::with_capacity(estimated_capacity);
-        let mut soundboard_pressure_coefficients = Vec::with_capacity(estimated_capacity);
-        let mut listener_pressure_coefficients = Vec::with_capacity(estimated_capacity);
+        for i in 1..=num_modes {
+            let inharmonicity = 0.0005;
+            let freq = base_frequency * i as f64 * (1.0 + inharmonicity * i as f64 * i as f64);
+            let angular_frequency = 2.0 * PI * freq;
 
-        let density_times_sound_speed_squared =
-            design.air_density * design.speed_of_sound * design.speed_of_sound;
+            let damping_ratio = (0.01 + 0.0005 * i as f64).clamp(1.0e-4, 0.5);
 
-        for index_x in 0..=maximum_index_x {
-            for index_y in 0..=maximum_index_y {
-                for index_z in 0..=maximum_index_z {
-                    if index_x + index_y + index_z == 0 {
-                        continue;
-                    }
+            let shape_sb = simplified_mode_shape(i, norm_pos_sb);
+            let shape_lis = simplified_mode_shape(i, norm_pos_lis);
 
-                    let wavenumber_x = index_x as f64 * PI * inverse_room_x;
-                    let wavenumber_y = index_y as f64 * PI * inverse_room_y;
-                    let wavenumber_z = index_z as f64 * PI * inverse_room_z;
+            modal_states.push(ComplexModalMode::new(angular_frequency, damping_ratio));
 
-                    let wavenumber_squared = wavenumber_x * wavenumber_x
-                        + wavenumber_y * wavenumber_y
-                        + wavenumber_z * wavenumber_z;
+            mode_shape_at_soundboard.push(shape_sb);
+            mode_shape_at_listener.push(shape_lis);
 
-                    let wavenumber = wavenumber_squared.sqrt();
+            soundboard_pressure_coefficients.push(2.0 * shape_sb);
+            listener_pressure_coefficients.push(2.0 * shape_lis);
 
-                    if wavenumber > maximum_wavenumber {
-                        continue;
-                    }
+            let damped_frequency = angular_frequency * (1.0 - damping_ratio * damping_ratio).sqrt();
+            let complex_eigenvalue =
+                Complex64::new(-damping_ratio * angular_frequency, damped_frequency);
 
-                    let angular_frequency = design.speed_of_sound * wavenumber;
+            let projection_denominator = Complex64::new(2.0, 0.0)
+                + complex_eigenvalue * (2.0 * damping_ratio / angular_frequency);
 
-                    let nonzero_index_count =
-                        (index_x > 0) as usize + (index_y > 0) as usize + (index_z > 0) as usize;
+            let pressure_source_scale = density_c2 / design.room_dimensions.x * shape_sb;
 
-                    let modal_volume = room_volume / (1usize << nonzero_index_count) as f64;
-
-                    let frequency = angular_frequency / (2.0 * PI);
-                    let reverberation_time = 0.30 + 0.55 * (-frequency / 160.0).exp();
-
-                    let damping_ratio =
-                        (6.9 / (angular_frequency * reverberation_time)).clamp(1.0e-4, 0.5);
-
-                    let shape_at_soundboard = mode_shape_at_position(
-                        index_x,
-                        index_y,
-                        index_z,
-                        design.soundboard_position,
-                        inverse_room_x,
-                        inverse_room_y,
-                        inverse_room_z,
-                    );
-
-                    let shape_at_listener = mode_shape_at_position(
-                        index_x,
-                        index_y,
-                        index_z,
-                        design.listener_position,
-                        inverse_room_x,
-                        inverse_room_y,
-                        inverse_room_z,
-                    );
-
-                    modal_states.push(ComplexModalMode::new(angular_frequency, damping_ratio));
-
-                    mode_shape_at_soundboard.push(shape_at_soundboard);
-                    mode_shape_at_listener.push(shape_at_listener);
-
-                    soundboard_pressure_coefficients.push(2.0 * shape_at_soundboard);
-                    listener_pressure_coefficients.push(2.0 * shape_at_listener);
-
-                    let damped_frequency =
-                        angular_frequency * (1.0 - damping_ratio * damping_ratio).sqrt();
-
-                    let complex_eigenvalue =
-                        Complex64::new(-damping_ratio * angular_frequency, damped_frequency);
-
-                    let projection_denominator = Complex64::new(2.0, 0.0)
-                        + complex_eigenvalue * (2.0 * damping_ratio / angular_frequency);
-
-                    let pressure_source_scale =
-                        density_times_sound_speed_squared / modal_volume * shape_at_soundboard;
-
-                    source_gain_per_volume_velocity.push(
-                        projection_denominator
-                            .inverse()
-                            .scaled(pressure_source_scale),
-                    );
-                }
-            }
+            source_gain_per_volume_velocity.push(
+                projection_denominator
+                    .inverse()
+                    .scaled(pressure_source_scale),
+            );
         }
 
         AirRoom {
